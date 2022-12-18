5da03b80610a23a6ec5d919c188cd2ec3cbab935
Zach Beiser <zbeiser@gmail.com>
2022-12-18 14:04:13 -0800

    completed manager column of View All Employees query

--- db/query.sql
+++ db/query.sql
@@ -1,10 +1,11 @@
 -- View All Roles table
-SELECT role.id, role.title, role.salary, department.name AS department
+SELECT role.id, role.title, department.name AS department, role.salary
 FROM role 
 JOIN department ON department.id = role.department_id;
 
 -- View All Employees table
-SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, employee.manager_id AS manager
-FROM employee
-JOIN role ON employee.role_id = role.id
-JOIN department ON role.department_id = department.id;
+SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager 
+FROM employee 
+LEFT JOIN role on employee.role_id = role.id 
+LEFT JOIN department on role.department_id = department.id 
+LEFT JOIN employee manager on manager.id = employee.manager_id;
